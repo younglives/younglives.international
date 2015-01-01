505b2f75768fc0ff3e832b62ef1ce2e18295e311
Michael Davis <m.r.davis@me.com>
2015-01-01 23:04:44 +0000

    PEP8

--- younglives/international/__init__.py
+++ younglives/international/__init__.py
@@ -2,5 +2,6 @@ from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('younglives.international')
 
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
--- younglives/international/browser/interfaces.py
+++ younglives/international/browser/interfaces.py
@@ -1,5 +1,6 @@
 from zope.interface import Interface
 
+
 class IYounglivesInternational(Interface):
     """Marker interface that defines a Zope 3 browser layer.
     """
